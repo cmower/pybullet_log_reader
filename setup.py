9be84ea7353d992e2021e7d43a17a77a21151f58
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-08-17 11:54:07 +0100

    1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="pybullet_log_reader",
     description="",
-    version="0.0.0",
+    version="1.0.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cmower/pybullet_log_reader",
